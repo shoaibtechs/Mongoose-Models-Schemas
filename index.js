aecc2504d1cfe6a51ad5a68e7538ac3d92fcae57
shoaibtechs <p200147@pwr.nu.edu.pk>
2026-02-05 18:17:56 +0500

    Add mongDb connection using mongoose

---
+++ index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+
+main()
+    .then( ()=>{
+
+        console.log("Connection Sucessfull");
+
+    })
+
+    .catch((err)=>{
+
+        console.log(err);
+
+
+})
+
+
+async function main(){
+
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+
+
+}
+
+
+const userSchema = new mongoose.Schema({
+
+    name: String,
+    email: String,
+    age : Number,  
+
+
+});
+
+const User = mongoose.model("User", userSchema);
