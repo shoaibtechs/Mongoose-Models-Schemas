b8bdcf623057ade95246009eeedf27706c8f5f3a
shoaibtechs <p200147@pwr.nu.edu.pk>
2026-02-10 01:18:15 +0500

    Rename index.js file

---
+++ index1.js
@@ -0,0 +1,147 @@
+const mongoose = require('mongoose');
+
+
+main()
+    .then( ()=>{
+
+        console.log("Connection Sucessfull");
+
+    })
+
+    .catch((err)=>{
+
+        console.log(err);
+
+
+})
+
+
+async function main(){
+
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+
+
+}
+
+
+// Creating a Schema 
+
+
+
+const userSchema = new mongoose.Schema({
+
+    name: String,
+    email: String,
+    age : Number,  
+
+
+});
+
+
+
+
+// Creating a Model/ Collection 
+
+
+let User = mongoose.model("User", userSchema);
+
+
+
+
+// Inserting One  in Mongoose 
+
+
+
+// let user1 =  new User ({
+
+//     name : "Shoaib",
+//     email : "shoaibbabar524@gmail.com",
+//     age: 48
+
+
+// })
+
+
+
+
+// user1.save()
+ 
+// .then((res)=>{
+
+//     console.log(res);
+
+
+// })
+
+// .catch((err)=>{
+
+//     console.log(err);
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Insert Many In Mongoose 
+
+
+
+User.insertMany([
+
+    {   
+        name: "Hashim",
+        email: "Hashim12@gmail.com",
+        age: 50
+    }, 
+
+    {
+
+        name: "Hashim", 
+        email : "Fawad@gmail.com", 
+        age : 77
+
+    },
+
+    {
+
+        name : "Imran Khan",
+        email : "imran@gmail.com",
+        age : 56
+
+    }
+
+
+])
+
+
+
+
+// .then((res)=>{
+
+//     console.log(res);
+
+
+// })
+
+// .catch((err)=>{
+
+//     console.log(err);
+
+// })
+
+
+
+
+
+
+
