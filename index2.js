1b22c3a5856ac48ca81f3ffef5c8b8031e270765
shoaibtechs <p200147@pwr.nu.edu.pk>
2026-02-10 01:47:57 +0500

    Add find with condtion in mongoose

--- index2.js
+++ index2.js
@@ -8,27 +8,47 @@ const User =  require("./index1"); // Getting the  user Model form the index1.js
 
 
 
-User.find({})
+// User.find({})
 
 
+// .then((res)=>{
+
+//     console.log(res)
+
+
+// })
+
+// .catch((err)=>{
+
+//     console.log(err);
+
+
+// })
+
+
+
+
+
+
+
+
+//2. Find with condition 
+
+
+User.find({age : {$gt : 50}})
 .then((res)=>{
 
-    console.log(res)
+    console.log(res);
 
 
 })
+
 
 .catch((err)=>{
 
     console.log(err);
 
-
 })
-
-
-
-
-
 
 
 
