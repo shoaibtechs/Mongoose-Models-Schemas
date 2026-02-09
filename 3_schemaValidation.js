511a48dfcd6c42dee9b29a3a469bb1f00576ccdc
shoaibtechs <p200147@pwr.nu.edu.pk>
2026-02-10 03:59:26 +0500

    Add new Schema Validate

---
+++ 3_schemaValidation.js
@@ -0,0 +1,66 @@
+let mongoose =  require("mongoose");
+
+
+
+async function main()
+{
+
+    await mongoose.connect('mongodb://127.0.0.1:27017/shoaibtechs');
+
+
+
+}
+
+
+main()
+
+.then(()=>{
+
+    console.log("Connection Sucessfull");
+
+})
+
+
+.catch((err)=>{
+
+    console.log("Can not connect with the Database");
+
+
+})
+
+
+
+
+
+
+
+const bookSchema = new mongoose.Schema({
+
+    title : {
+
+        type : String,
+        required : true 
+
+    }, 
+
+    author : {
+
+        type : String 
+
+    }, 
+
+    price : {
+
+        type: Number
+
+    }
+
+
+
+})
+
+
+
+const Book = mongoose.model("Book", bookSchema);
+
+
